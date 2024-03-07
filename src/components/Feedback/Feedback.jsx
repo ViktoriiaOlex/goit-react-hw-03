94866cdb0fdd03461165af4947dc150d0c51fd32
Victoriia Oleh <142164676+ViktoriiaOlex@users.noreply.github.com>
2024-03-07 19:55:37 +0300

    Initial commit

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,13 @@
+const Feedback = ({ rating, totalFeedback, averageRate }) => {
+    return (
+    <>
+        <p>Good: {rating.good}</p>
+        <p>Neutral: {rating.neutral}</p>
+        <p>Bad: {rating.bad}</p>
+        <p>Total: {totalFeedback}</p>
+        <p>Positive: {averageRate}%</p>
+    </>
+    );
+};
+
+export default Feedback;
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,35 @@
+const Options = ({ handleFeedback, handleResetRating, summaryRating }) => {
+    return (
+    <div>
+        <button
+        type="button"
+        onClick={() => {
+            handleFeedback('good');
+        }}
+        >
+        Good
+        </button>
+        <button type="button"
+        onClick={() => { 
+        handleFeedback('neutral');
+        }}
+        >
+        Neutral
+        </button>
+        <button type="button"
+        onClick={() => {
+        handleFeedback('bad');
+        }}
+        >
+        Bad
+        </button>
+        {summaryRating > 0 ? (
+        <button type="button" onClick={handleResetRating}>
+        Reset
+        </button>
+        ) : null}
+    </div>
+    );
+};
+
+export default Options;
